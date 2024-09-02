5a01c1f0c9f7b327ac0481acd2fddf3607a9c9a3
Sariot <sariothossain1011>
2024-09-02 16:33:11 +0600

    wishlist page

--- app/cart/page.tsx
+++ app/cart/page.tsx
@@ -89,17 +89,13 @@ const CartPage: React.FC = () => {
         return cartData.reduce((acc, item) => acc + calculateTotalPrice(item.price, item.quantity), 0);
     };
 
-    //
-    const removeCartItem=(id:number)=>{
-
+    //remove Cart Item
+    const removeCartItem = (id: number) => {
         setCartData(prevCartData =>
-            prevCartData.map(item =>
-                item.id === id && item.quantity > 1
-                    ? { ...item, quantity: item.quantity - 1 }
-                    : item
-            )
+            prevCartData.filter(item => item.id !== id)
         );
-    }
+    };
+    
 
     return (
         <div className='container'>
---
+++ app/wishlist/page.tsx
@@ -0,0 +1,84 @@
+'use client'
+import Link from 'next/link'
+import Img from "@/public/instagram/insta01.jpeg"
+import React, { useState } from 'react'
+import { RiCalendarTodoFill } from "react-icons/ri";
+import { RiDeleteBin5Line } from "react-icons/ri";
+import Image from 'next/image';
+import QuickViewWishlistModal from '@/components/common/QuickViewWishlistModal';
+interface Wishlist {
+    id: number,
+    title: string,
+    date: string,
+    quantity: number,
+    price: number,
+    image: string,
+}
+
+
+const WishlistPage = () => {
+    const [showModal, setShowModal] = useState(true);
+    const [wishlist, setWishlist] = useState<Wishlist[]>([
+        {
+            id: 1,
+            title: "Cropped Blazer",
+            date: "SEP 02, 2024",
+            quantity: 2,
+            price: 350,
+            image: Img.src
+        },
+        {
+            id: 2,
+            title: "Cropped Blazer",
+            date: "SEP 02, 2024",
+            quantity: 5,
+            price: 500,
+            image: Img.src
+        }
+    ]);
+
+    //remove wishlist cart data
+    const removeWishLIstItem = (id: number) => {
+        setWishlist(prevWishlistData =>
+            prevWishlistData.filter(item => item.id !== id)
+        );
+    };
+    const handleModalClose = () => {
+        setShowModal(false);
+    };
+    return (
+        <div className='container'>
+            {showModal && (
+                <QuickViewWishlistModal
+                    onClose={handleModalClose}
+                />)
+            }
+            <div className=' text-center py-20 md:py-40'>
+                <h1 className=' text-2xl md:text-4xl font-semibold md:font-medium uppercase'>Page Wishlist</h1>
+                <p className='text-sm font-normal py-2'><Link href="/" >Home</Link> &#x2022; <span>Page Wishlist</span></p>
+            </div>
+            <div className='flex flex-col gap-4 pb-10 md:pb-20'>
+                {
+                    wishlist.map((item, index) => (
+                        <div key={index} className=' flex flex-row justify-between items-center rounded-xl border p-4 md:p-8 hover:shadow-xl duration-300'>
+                            <div className=' flex flex-row items-center gap-4'>
+                                <RiDeleteBin5Line size={18} className=' cursor-pointer' onClick={() => removeWishLIstItem(item.id)} />
+                                <Image src={item.image} alt={item.title} width={100} height={100} className=' rounded-lg' />
+                                <div className='text-[12px] text-gray-700'>
+                                    <p className=' font-bold'>{item.title}</p>
+                                    <p>${item.price}</p>
+                                    <p className='flex gap-2 items-center'><RiCalendarTodoFill size={14} /><span >{item.date}</span></p>
+                                </div>
+                            </div>
+                            <button className='w-48  py-4 text-center hover:bg-opacity-95 bg-[#132842] text-white rounded-full text-sm font-semibold'>
+                                Quick View
+                            </button>
+                        </div>
+                    ))
+                }
+            </div>
+        </div>
+    )
+}
+
+export default WishlistPage
