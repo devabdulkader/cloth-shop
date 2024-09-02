961dc33ee640d85d899f2269f29cce68f70dec1a
Abdur-Rouf-Jibon <rouf.uiux@gmail.com>
2024-09-02 17:16:25 +0600

    fallback:scrollbar fixed

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -30,13 +30,7 @@ const config: Config = {
         "2xl": "1350px",
       },
     },
-    extend: {
-      backgroundImage: {
-        "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
-        "gradient-conic":
-          "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
-      },
-    },
+    extend: {},
   },
   plugins: [],
 };
