939269a41eafdedea245df4ce5bdcbef371084c2
Abdur-Rouf-Jibon <rouf.uiux@gmail.com>
2024-09-04 22:40:11 +0600

    added loader

---
+++ app/products/[productId]/loading.tsx
@@ -0,0 +1,12 @@
+import Loader from "@/components/common/Loader";
+import React from "react";
+
+const Loading = () => {
+  return (
+    <div>
+      <Loader />
+    </div>
+  );
+};
+
+export default Loading;
